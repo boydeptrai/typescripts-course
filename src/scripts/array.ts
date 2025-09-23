d27191adef80170390caf38bbaae9e8842b28d31
boydeptrai <langthamht047@gmail.com>
2025-09-23 20:55:13 +0700

    feat(course): array type

---
+++ src/scripts/array.ts
@@ -0,0 +1,16 @@
+const hobbies: string[] = ["badminton", "soccer","basketball"];
+const scores: number[] = [1,2,3,4];
+const filter: boolean[] = [false, true];
+const students: {
+    name?: string;
+    age?: number;
+}[] = [
+    {
+        name: "tuan tran",
+        age:29,
+    },
+    {
+        name: "thanh hoa",
+        age: 22,
+    },
+]
